7166ddda5f9844e3ad19ec27270e4a8cc7215deb
Iftee97 <tawsifmasrur@gmail.com>
2022-07-07 16:09:23 +0600

    ...

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -60,6 +60,22 @@
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 8.46967C5.76256 8.17678 6.23744 8.17678 6.53033 8.46967L10.2676 12.2069C10.6724 12.6117 10.9444 12.883 11.1708 13.0751C11.3904 13.2615 11.5171 13.329 11.6137 13.3604C11.8648 13.442 12.1352 13.442 12.3863 13.3604C12.4829 13.329 12.6096 13.2615 12.8292 13.0751C13.0556 12.883 13.3276 12.6117 13.7324 12.2069L17.4697 8.46967C17.7626 8.17678 18.2374 8.17678 18.5303 8.46967C18.8232 8.76256 18.8232 9.23744 18.5303 9.53033L14.7931 13.2676L14.7715 13.2891C14.3938 13.6668 14.0786 13.9821 13.8 14.2186C13.5094 14.4653 13.2098 14.67 12.8498 14.787C12.2975 14.9664 11.7025 14.9664 11.1502 14.787C10.7902 14.67 10.4906 14.4653 10.2 14.2186C9.9214 13.9821 9.60615 13.6668 9.22846 13.2891L9.20693 13.2676L5.46967 9.53033C5.17678 9.23744 5.17678 8.76256 5.46967 8.46967Z"></path>
               </svg>
             </button>
+
+            <div id="dropdown-button" class="absolute right-0 mt-3 w-48 divide-y divide-gray-100 rounded-lg border border-gray-100 bg-white shadow-xl">
+              <div class="px-4 py-3" role="none">
+                <p class="text-sm font-medium text-gray-500" role="none">Signed in as</p>
+                <p class="truncate text-sm font-semibold text-gray-900" role="none">example@email.com</p>
+              </div>
+
+              <div class="py-1">
+                <a href="/onboarding" class="flex items-center px-4 py-2 text-sm font-medium text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Create Blog</a>
+                <a href="/billing" class="flex items-center px-4 py-2 text-sm font-medium text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Billing & Plans</a>
+              </div>
+
+              <div class="py-1">
+                <button type="submit" class="flex w-full items-center px-4 py-2 text-sm font-medium text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900" role="menu-item">Logout</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
